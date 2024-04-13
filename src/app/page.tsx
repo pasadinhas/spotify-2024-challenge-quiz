5f5ce21a702f9e5b4e173226b0217fbe0b0236d0
Miguel Pasadinhas <pasadinhas@Miguels-MacBook-Pro.local>
2024-04-13 11:57:55 +0100

    feat: first draft of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,158 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
+import Data from "./Data"
+
+const {Alphabet,ArtistChallengeData} = Data
+
+type Tracks = typeof ArtistChallengeData['A']
+type Track = Tracks['values']
+
+type Answers = {[letter: string]: Answer}
+
+type Answer = {
+  locked: Boolean,
+  answers: string[]
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const defaultAnswers: Answers = {}
+  const [currentLetter, setCurrentLetter] = useState('A')
+  const [answers, setAnswers]: [Answers, (arg0: Answers) => void] = useState(() => 
+    Alphabet.reduce((result, letter) => {
+      result[letter] = {
+        locked: false,
+        answers: [],
+      }
+      return result
+    }, defaultAnswers)
+  )
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const setAnswerForCurrentLetterWithIndex = (answer: string, index: number) => {
+    if (answers[currentLetter].locked) {
+      console.debug("Cannot change answers because they have already been submited for: " + currentLetter)
+      return
+    }
+    for (let i = 0; i < answers[currentLetter].answers.length; i += 1) {
+      if (answers[currentLetter].answers[i] == answer) {
+        delete answers[currentLetter].answers[i]
+      }
+    }
+    answers[currentLetter].answers[index] = answer
+    setAnswers({...answers})
+  }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  function guess() {
+    const numberOfAnswers = answers[currentLetter].answers.filter(Boolean).length
+    if (numberOfAnswers < 3) {
+      alert("Oops, you didn't select answers for all tracks.")
+      return
+    }
+    answers[currentLetter].locked = true
+    setAnswers({...answers})
+  }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+  return (<>
+    <header className="flex justify-center p-10">
+      <Progress currentLetter={currentLetter} setCurrentLetter={setCurrentLetter}/>
+    </header>
+    <main className="flex flex-col min-w-screen p-10 gap-10">
+      <ThreeColumns>
+        <IndividualTrackQuiz tracks={ArtistChallengeData[currentLetter]} index={0} answers={answers[currentLetter]} setAnswerForCurrentLetterWithIndex={setAnswerForCurrentLetterWithIndex} />
+        <IndividualTrackQuiz tracks={ArtistChallengeData[currentLetter]} index={1} answers={answers[currentLetter]} setAnswerForCurrentLetterWithIndex={setAnswerForCurrentLetterWithIndex} />
+        <IndividualTrackQuiz tracks={ArtistChallengeData[currentLetter]} index={2} answers={answers[currentLetter]} setAnswerForCurrentLetterWithIndex={setAnswerForCurrentLetterWithIndex} />
+      </ThreeColumns>
+      <Controls currentLetter={currentLetter} setCurrentLetter={setCurrentLetter} guess={guess}></Controls>
     </main>
+    </>
   );
+}
+
+function Controls({currentLetter, setCurrentLetter, guess}) {
+  const previousLetter = currentLetter == "A" ? "Z" : String.fromCharCode(currentLetter.charCodeAt(0) - 1);
+  const nextLetter = currentLetter == "Z" ? "A" : String.fromCharCode(currentLetter.charCodeAt(0) + 1);
+  const btnClasses = "w-36 border border-neutral-500 px-5 py-3 hover:outline outline-offset-2 outline-neutral-300"
+  return <div className="flex justify-center gap-10 my-5">
+    <button className={btnClasses} onClick={() => setCurrentLetter(previousLetter)}>Previous</button>
+    <button className={btnClasses} onClick={guess}>Guess</button>
+    <button className={btnClasses} onClick={() => setCurrentLetter(nextLetter)}>Next</button>
+  </div>
+}
+
+function ThreeColumns({ children }: { children: string | JSX.Element | JSX.Element[] }) {
+  return <div className="grid grid-rows-1 grid-cols-3 gap-x-20">
+    {children}
+  </div>
+}
+
+function Progress({currentLetter, setCurrentLetter}: {currentLetter: string, setCurrentLetter: (_: string) => void}) {
+  const selectedClasses = "outline outline-offset-2 outline-neutral-300"
+  return <div className="flex items-center">
+    {Alphabet.map(letter => <>
+      <span className={`p-2 h-10 font-bold text-center border border-dotted border-neutral-500 border-r-0 last:border-r aspect-square cursor-pointer ${letter == currentLetter ? selectedClasses : ""}`} onClick={() => setCurrentLetter(letter)}>{letter}</span>
+    </>)}
+  </div>
+}
+
+type IndividualTrackQuizProps = { tracks: Tracks, index: number, answers: Answer, setAnswerForCurrentLetterWithIndex: (answer: string, index: number) => void }
+function IndividualTrackQuiz({tracks, index, answers, setAnswerForCurrentLetterWithIndex}: IndividualTrackQuizProps) {
+  if (tracks.length < index + 1) {
+    return <></>
+  }
+
+  const options = tracks.map(track => track.addedBy.data).sort((a, b) => a.name.localeCompare(b.name))
+
+  return <div className="grid gap-10 select-none">
+    <SpotifyWidget songId={tracks[index].itemV2.data.uri} />
+    <TrackAddedByOptions options={options} selectedAnswer={answers.answers[index]} locked={answers.locked} setAnswerForTrack={(answer: string) => setAnswerForCurrentLetterWithIndex(answer, index)} />
+  </div>
+}
+
+function SpotifyWidget({ songId }: { songId: string }) {
+  return <div className="relative">
+    <iframe // this iframe is used just for the background color using blur
+      className="absolute translate-y-full overflow-hidden blur-3xl -z-10 scale-y-[3] saturate-50 opacity-30"
+      src={`https://open.spotify.com/embed/track/${songId.replace("spotify:track:", "")}?utm_source=generator`}
+      width={'100%'}
+      height="160" 
+      frameBorder="0" 
+      allowFullScreen={false} 
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+      loading="lazy">
+    </iframe>
+    <iframe // this is the actual iframe that will show up
+      className="rounded-xl"
+      src={`https://open.spotify.com/embed/track/${songId.replace("spotify:track:", "")}?utm_source=generator`}
+      width={'100%'}
+      height="160" 
+      frameBorder="0" 
+      allowFullScreen={false} 
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+      loading="lazy">
+    </iframe>
+  </div>
+}
+
+
+
+type TrackAddedByOptionsProps = { options: Track.addedBy.data }
+function TrackAddedByOptions({ options, selectedAnswer, setAnswerForTrack, locked }) {
+  return <div className="grid gap-5">
+    <Option option={options[0]} selectedAnswer={selectedAnswer} selectAnswer={() => setAnswerForTrack(options[0]?.name)} />
+    <Option option={options[1]} selectedAnswer={selectedAnswer} selectAnswer={() => setAnswerForTrack(options[1]?.name)} />
+    <Option option={options[2]} selectedAnswer={selectedAnswer} selectAnswer={() => setAnswerForTrack(options[2]?.name)} />
+  </div>
+}
+
+
+function Option({ option, selectedAnswer, selectAnswer, locked }) {
+  if (!option) return <></>
+  // const backgroundColor = (!locked && selectedAnswer == option.name)
+  const selectedClassName = selectedAnswer == option.name ? "bg-neutral-500/50" : ""
+  return <div className={`flex p-5 cursor-pointer rounded-xl border border-dashed border-neutral-500 ${selectedClassName} outline-offset-4 outline-neutral-300 hover:outline`} onClick={selectAnswer}>
+    <img className="w-10 h-10 rounded-xl align-middle	" src={option.avatar.sources[0].url} />
+    <span className="leading-10 ps-5 text-center">{option.name}</span>
+  </div>
+  
 }
