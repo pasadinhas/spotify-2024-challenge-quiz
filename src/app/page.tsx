e6a7eaa8147bd38b4541f681ba48e6b94e6cfd58
Miguel Pasadinhas <pasadinhas@Miguels-MacBook-Pro.local>
2024-04-13 12:34:38 +0100

    feat: add colors to show if answers are correct or incorrect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,9 +143,22 @@ type OptionProps = { track: Track, option: AddedBy, answers: Answer, setAnswerFo
 function Option({ track, option, answers, setAnswerForCurrentLetterTrack }: OptionProps) {
   if (!option) return <></>
   const selectedAnswer = answers.selected[track.uid]
-  // const backgroundColor = (!locked && selectedAnswer == option.name)
-  const selectedClassName = selectedAnswer == option.name ? "bg-neutral-500/50" : ""
-  return <div className={`flex p-5 cursor-pointer rounded-xl border border-dashed border-neutral-500 ${selectedClassName} outline-offset-4 outline-neutral-300 hover:outline`} 
+  const correctAnswer = track.addedBy.data.name
+  const isThisOptionSelected = selectedAnswer == option.name
+  const isThisOptionCorrect = correctAnswer == option.name
+  const areAnswersLocked = answers.locked
+
+  let backgroundColor = ""
+
+  if (areAnswersLocked && isThisOptionSelected && !isThisOptionCorrect) {
+    backgroundColor = "bg-red-500/50"
+  } else if (areAnswersLocked && isThisOptionCorrect) {
+    backgroundColor = "bg-green-500/50"
+  } else if (isThisOptionSelected) {
+    backgroundColor = "bg-neutral-500/50"
+  }
+
+  return <div className={`flex p-5 cursor-pointer rounded-xl border border-dashed border-neutral-500 ${backgroundColor} outline-offset-4 outline-neutral-300 hover:outline`} 
     onClick={() => setAnswerForCurrentLetterTrack(option?.name, track)}>
     <img className="w-10 h-10 rounded-xl align-middle	" src={option.avatar?.sources[0].url} />
     <span className="leading-10 ps-5 text-center">{option.name}</span>
